e858b231603c49e8fdb6a536aafdc461af2baec4
Karmel920 <mikolajpe2@gmail.com>
2022-12-07 06:56:27 +0100

    day7

---
+++ day07/check.py
@@ -0,0 +1,52 @@
+import pytest as pytest
+from part1 import compute as compute1
+from part2 import compute as compute2
+
+INPUT_S = '''\
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+'''
+
+EXPECTED1 = 95437
+EXPECTED2 = 24933642
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED1),
+    ),
+)
+def test1(input_s: str, expected: int) -> None:
+    assert compute1(input_s) == expected
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED2),
+    ),
+)
+def test2(input_s: str, expected: int) -> None:
+    assert compute2(input_s) == expected
---
+++ day07/part2.py
@@ -0,0 +1,83 @@
+import argparse
+import os
+from collections import defaultdict
+from copy import deepcopy
+
+import pytest as pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+INPUT_S = '''\
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+'''
+
+
+def compute(input_s: str) -> int:
+    sizes = defaultdict(int)
+    path = []
+    lines = input_s.strip().split('\n')
+    for line in lines:
+        words = line.strip().split()
+
+        if words[1] == 'cd':
+            if words[2] == '..':
+                path.pop()
+            else:
+                path.append(words[2])
+        elif words[1] == 'ls':
+            continue
+        elif words[0] == 'dir':
+            continue
+        else:
+            size = int(words[0])
+            for i in range(1, len(path) + 1):
+                sizes['/'.join(path[:i])] += size
+
+    max_ = 40000000
+    total = sizes['/'] - max_
+    min_ = 10000000
+
+    for k, v in sizes.items():
+        if v >= total:
+            if v < min_:
+                min_ = v
+
+    return min_
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        # print(compute(INPUT_S))
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
