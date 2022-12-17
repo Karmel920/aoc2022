f5767147a57108643b3d2977bf400664035b3a4b
Karmel920 <mikolajpe2@gmail.com>
2022-12-17 18:59:28 +0100

    day15

---
+++ day15/check.py
@@ -0,0 +1,43 @@
+import pytest as pytest
+from part1 import compute as compute1
+from part2 import compute as compute2
+
+INPUT_S = '''\
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+'''
+
+EXPECTED1 = 26
+EXPECTED2 = 56000011
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED1),
+    ),
+)
+def test1(input_s: str, expected: int) -> None:
+    assert compute1(input_s) == expected
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED2),
+    ),
+)
+def test2(input_s: str, expected: int) -> None:
+    assert compute2(input_s) == expected
